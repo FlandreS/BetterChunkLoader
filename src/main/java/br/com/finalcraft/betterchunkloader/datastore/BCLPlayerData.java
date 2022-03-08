12052b5ee3c923717053a38f86dccca9e015dd89
Petrus <petrus_ant@hotmail.com>
2022-03-08 16:18:44 -0300

    Ops, this should not be in here

--- src/main/java/br/com/finalcraft/betterchunkloader/datastore/BCLPlayerData.java
+++ src/main/java/br/com/finalcraft/betterchunkloader/datastore/BCLPlayerData.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.UUID;
 
-@XmlRootElement(name = "PlayersData")
+@XmlRootElement
 public class BCLPlayerData {
 	private UUID playerId;
 	private int alwaysOnChunksAmount, onlineOnlyChunksAmount;
